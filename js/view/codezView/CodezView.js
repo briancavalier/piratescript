81f866982767d96a0f6fef872b4978084e79fad7
Brian Cavalier <brian@briancavalier.com>
2011-04-30 23:35:55 -0400

    Simple rendering with string replacement in CodezView

--- js/view/codezView/CodezView.js
+++ js/view/codezView/CodezView.js
@@ -6,12 +6,18 @@ define(
 ],
 function(when, template) {
 
+	var undef;
+
 	function CodezView(node) {
 		this.node = node;
-		node.innerHTML = template;
+		this.render();
+		// node.innerHTML = template;
 	};
 	
 	CodezView.prototype = {
+		render: function() {
+			this.node.innerHTML = template.replace(/\$\{(\w+)\}/, function(s, key) { return this[key] === undef ? '' : this[key]; });
+		},
 		newCodez: function(data) {
 			var d = when.Deferred();
 			
