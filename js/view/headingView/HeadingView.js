ff6828237ebc9c834f679f1a7524e24daf3b8dc2
unscriptable <john@e-numera.com>
2011-05-01 22:39:46 -0400

    created new HeadingView

---
+++ js/view/headingView/HeadingView.js
@@ -0,0 +1,27 @@
+define(
+[
+	'text!./HeadingView.html',
+	'cssx/css!./HeadingView.css'
+],
+function (template) {
+
+	var undef;
+
+	function HeadingView(node) {
+		this.node = node;
+		this.render();
+	}
+
+	HeadingView.prototype = {
+
+		render: function(map) {
+			this.node.innerHTML = template.replace(/\$\{(\w+)\}/g, function(s, key) {
+				return map && map[key] !== undef ? map[key] : '';
+			});
+		}
+
+	};
+
+	return HeadingView;
+
+});
