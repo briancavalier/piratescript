90655fd2c66559f10fb46e036e76b643dc3ce901
unscriptable <john@e-numera.com>
2011-05-01 22:41:42 -0400

    got it all working together

--- js/controller/PiratescriptController.js
+++ js/controller/PiratescriptController.js
@@ -1,8 +1,18 @@
-define(['when'], function(when) {
+define(
+	[
+		'when',
+		'css!../css/gray.css'
+	],
+	function(when) {
 	
 	function Controller() {}
 	
 	Controller.prototype = {
+
+		data: null,
+
+		questionNum: 0,
+
 		ready: function() {
 			this._showNextCodez();
 		},
@@ -10,22 +20,22 @@ define(['when'], function(when) {
 		_showNextCodez: function() {
 			var self = this;
 			when(this._getCodez()).then(function(codez) {
-				var promise = self._codezView.showCodez(codez.content);
-				
+				var promise = self._codezView.showCodez(codez[self.questionNum]);
+
+				self.questionNum = (self.questionNum + 1) % self.data.length;
 				// When the view resolves the promise, check the answer and display
 				// the next one.  Probably want a counter here so we can show a final
 				// score screen after N questions.
 				promise.then(function(answer) {
 					self._checkAnswer(codez, answer);
 					
-					setTimeout(function() { self._showNextCodez(); });
+					setTimeout(function() { self._showNextCodez(); }, 0);
 				});
 			});
 		},
 		
 		_getCodez: function() {
-			// TODO: Get some real new codez here
-			return { content: "codez go here", pirate: true };
+			return this.data;
 		},
 		
 		_checkAnswer: function(codez, answer) {
--- js/spec.js
+++ js/spec.js
@@ -1,12 +1,22 @@
 define({
 	plugins: [
 		{ module: 'wire/debug' },
-		{ module: 'wire/dom' }
+		{ module: 'wire/dom' },
+		{ module: 'wire/sizzle' }
 	],
+	data: {
+		module: '../data/codez'
+	},
 	codezView: {
 		create: {
 			module: 'view/codezView/CodezView',
 			args: { $ref: 'dom!codez-view' }
+		}
+	},
+	headingView: {
+		create: {
+			module: 'view/headingView/HeadingView',
+			args: { $ref: 'dom!heading-view' }
 		}
 	},
 	scoreView: {
@@ -18,7 +28,8 @@ define({
 	controller: {
 		create: 'controller/PiratescriptController',
 		properties: {
-			_codezView: { $ref: 'codezView' }
+			_codezView: { $ref: 'codezView' },
+			data: { $ref: 'data' }
 		},
 		init: 'ready'
 	}
